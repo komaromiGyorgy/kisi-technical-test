442e1e30498551cb4d32c1aaa3954beb72479626
Komi <komaromigy40@gmail.com>
2023-05-17 21:25:30 +0300

    KTT-4 Prepare backend for railway deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/ping", (_req, res) => {
   console.log("someone pinged here!!");
